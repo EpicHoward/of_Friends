fa919c421f6f0988c76bb61b202fd6044eea080c
andreGarvin <andregarvin718@gmail.com>
2017-06-01 21:24:39 -0400

    chaning the logic in the index.js file

--- src/app/index.js
+++ src/app/index.js
@@ -6,77 +6,92 @@ import sendNewLetter from './js/newsletter';
 import firebasedb from './js/firebase';
 import newsfeed from './js/newsfeed';
 
+var main = {
+    user_data: undefined,
+    showNewsFeed: function() {
+
+          if ( Object.keys( this.user_data.posts ).length !== 0 ) {
+
+              $('.newsfeed-container').html('');
+
+              for ( var j = this.user_data.posts.length; j > 0; j-- ) {
+
+                  var post = his.user_data.posts[j - 1];
+                  $('.newsfeed-container').append(`
+                      <div class='post col-xs-5 col-sm-5 col-md-8 col-xs-offset-1 col-md-offset-3'>
+                          <p class='pull-right'>${ post.date }</p>
+
+                          <img style='height: 50px; width: 50px;' class='img img-rounded' src=${ user_data.profile.profilePic !== '' ? user_data.profile.profilePic: 'http://alumni.harvard.edu/sites/default/files/styles/trip_photo/public/trip/main_photo/panada.png?itok=vPVFcRTG' }
+                          <p class='pull-left'>${ this.user_data.profile.user_name }</p>
+                          <h3>${ post.title }<h3>
+                          <p class='lead text-center'>${ post.content }</p>
+                      </div>
+                 `);
+               }
+         }
+         else {
+
+              $('#ui-message').html('You have no posts, you should make one');
+        }
+    },
+    displayName: function() {
+
+        $('#user_name').html(this.user_data.profile.user_name);
+        $('#email').html(this.user_data.profile.email);
+    },
+    makePost: function() {
+
+        var title = $('#title').val();
+        var content = $('#content').val();
+        if ( title.length !== 0 && content.length !== 0 ) {
+
+            user_data.posts = firebasedb.saveTo(`usersdb/${ user_data.profile.user_name }/posts/${ Object.keys( user_data.posts ).length }`,
+            {
+                id: Object.keys( user_data.posts ).length,
+                title: title,
+                content: content
+            });
+
+            $('#title').val('');
+            $('#content').val();
+        }
+    }
+};
 
 $('#app').hide();
 const auth = firebasedb.firebase.auth();
 
 firebasedb.firebase.auth().onAuthStateChanged(firebaseUser => {
 
-    var userObj;
     if ( firebaseUser ) {
 
-      userObj = {
+      var userObj = {
           user_name: firebaseUser.displayName,
           email: firebaseUser.email
       };
 
       firebasedb.getUser( userObj, (err, user_data ) => {
-          $('#app').show();
 
-          // console.log(`You are now logged in ${ firebaseUser.email }`);
+              $('#app').show();
+              console.log(`You are now logged in ${ firebaseUser.email }`);
 
-          $(document).ready(function() {
+              main.user_data = user_data;
+              $(document).ready(function() {
 
-              $('#user_name').html(user_data.profile.user_name);
-              $('#email').html(user_data.profile.email);
+                    main.displayName();
 
-              if ( Object.keys( user_data.posts ).length !== 0 ) {
+                    main.showNewsFeed();
 
-                  $('.newsfeed-container').html('');
+                    $('#logout').click(function() {
 
-                  for ( var j = user_data.posts.length; j > 0; j-- ) {
+                          return firebasedb.firebase.auth().signOut();
+                    });
 
-                      var post = user_data.posts[j - 1];
-                      $('.newsfeed-container').append(`
-                        <div class='post col-xs-5 col-sm-5 col-md-8 col-xs-offset-1 col-md-offset-3'>
-                             <p class='pull-right'>${ post.date }</p>
-                             <img style='height: 50px; width: 50px;' class='img img-rounded' src=${ user_data.profile.profilePic !== '' ? user_data.profile.profilePic: 'http://alumni.harvard.edu/sites/default/files/styles/trip_photo/public/trip/main_photo/panada.png?itok=vPVFcRTG' }
-                             <p class='pull-left'>${ user_data.profile.user_name }</p>
-                             <h3>${ post.title }<h3>
-                             <p class='lead text-center'>${ post.content }</p>
-                        </div>
-                      `);
-                  }
-              }
-              else {
+                    $('#submit').click(function () {
 
-                  $('#ui-message').html('You have no posts, you should make one');
-              }
-
-              $('#logout').click(function() {
-
-                  return firebasedb.firebase.auth().signOut();
+                          main.makePost();
+                    });
               });
-
-
-              $('#submit').click(function () {
-
-                  var title = $('#title').val();
-                  var content = $('#content').val();
-                  if ( title.length !== 0 && content.length !== 0 ) {
-
-                      user_data.posts = firebasedb.saveTo(`usersdb/${ user_data.profile.user_name }/posts/${ Object.keys( user_data.posts ).length }`,
-                      {
-                          id: Object.keys( user_data.posts ).length,
-                          title: title,
-                          content: content
-                      });
-
-                      $('#title').val('');
-                      $('#content').val();
-                  }
-              });
-          });
       });
 
     }
